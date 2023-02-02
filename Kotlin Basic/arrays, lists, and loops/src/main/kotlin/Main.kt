394663806b60ef9c28492e9e006e25b132924b0c
stitou-nada <nadastito1087@gmail.com>
2023-02-02 19:29:36 +0100

    array and loop and list

--- Kotlin Basic/arrays, lists, and loops/src/main/kotlin/Main.kt
+++ Kotlin Basic/arrays, lists, and loops/src/main/kotlin/Main.kt
@@ -1,5 +1,23 @@
 fun main() {
-    println("Hello World!")
+
+    //listOf cannot changed 
+    val school1 = listOf("mackerel", "trout", "halibut")
+    println(school1)
+
+    //can mutableListOf changed
+     val myList = mutableListOf("tuna", "salmon", "shark")
+    myList.remove("shark")
+     println(myList)
+
+    //arrayOf
+    val school = arrayOf("shark", "salmon", "minnow")
+    println(java.util.Arrays.toString(school))
+
+    // Loop 
+    val school2 = arrayOf("shark", "salmon", "minnow")
+    for (element in school2) {
+    print(element + " ")
+    }
 
 
 }
