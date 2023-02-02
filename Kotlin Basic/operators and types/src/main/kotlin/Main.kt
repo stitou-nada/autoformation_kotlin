4bda4f9bc87b595d591f63789eeee111a39bd744
stitou-nada <nadastito1087@gmail.com>
2023-02-02 19:08:52 +0100

    operation and typrs

--- Kotlin Basic/operators and types/src/main/kotlin/Main.kt
+++ Kotlin Basic/operators and types/src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
 fun main() {
-    //peration
+    //operation
     println(1+1)
     println(1-112)
     println(2/12)
     println(2*2)
+    
+    // types 
+    var i : Int = 4
+    val b1 = i.toString()
+    println(b1)
+    val b2 = i.toFloat()
+    println(b2)
 
+    //other types 
 
 }
