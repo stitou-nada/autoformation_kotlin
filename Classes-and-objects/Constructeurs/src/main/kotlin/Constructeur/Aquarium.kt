65b47ae1cdfd4544385e64fe4f80ad6db13d54b1
stitou-nada <nadastito1087@gmail.com>
2023-02-15 11:07:12 +0100

    constructeur secondaire

--- Classes-and-objects/Constructeurs/src/main/kotlin/Constructeur/Aquarium.kt
+++ Classes-and-objects/Constructeurs/src/main/kotlin/Constructeur/Aquarium.kt
@@ -6,5 +6,21 @@ class Aquarium( var length : Int = 100 , var width : Int=20 ,var height:Int = 40
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
+        println("Volume: $volume l")
     }
+    constructor(numberOfFish: Int) : this() {
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
+    var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+    /*init {
+        println("aquarium initializing")
+    }
+
+     */
 }
--- Classes-and-objects/Constructeurs/src/main/kotlin/Constructeur/main.kt
+++ Classes-and-objects/Constructeurs/src/main/kotlin/Constructeur/main.kt
@@ -15,6 +15,12 @@ fun BuildAquarium(){
     // everything custom
     val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
     aquarium4.printSize()
+ val aquarium6 = Aquarium(numberOfFish = 29)
+ aquarium6.printSize()
+ aquarium6.volume = 70
+ aquarium6.printSize()
+    //println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} l")
+
 }
 fun main (){
     BuildAquarium()
