424e1d5076eaf0b52f11075fd139a237552ab118
stitou-nada <nadastito1087@gmail.com>
2023-03-02 09:55:16 +0100

    Adaptation

--- Lecon 5  Mises en page/Tip Time/app/src/main/java/com/example/xml/MainActivity.kt
+++ Lecon 5  Mises en page/Tip Time/app/src/main/java/com/example/xml/MainActivity.kt
@@ -5,37 +5,40 @@ import android.os.Bundle
 import com.example.xml.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
+
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.calculateButton.setOnClickListener{ calculateTip() }
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
     }
-    fun calculateTip() {
+
+    private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
         if (cost == null) {
+            binding.tipResult.text = ""
             return
         }
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when (selectedId) {
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
+
         var tip = tipPercentage * cost
-        val roundUp = binding.roundUpSwitch.isChecked
-        if (roundUp) {
+        if (binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
         }
-        NumberFormat.getCurrencyInstance()
+
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-
-
     }
 }
