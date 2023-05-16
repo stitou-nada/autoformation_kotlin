4507222091431af504a1670f2a75ebeb2724f892
stitou-nada <nadastito1087@gmail.com>
2023-05-16 12:06:29 +0100

    maps

---
+++ Collections en Kotlin/Collections/src/main/kotlin/maps.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val peopleAges = mutableMapOf<String, Int>(
+        "Fred" to 30,
+        "Ann" to 23
+    )
+    //ajouter d'autres entrées à la map, vous pouvez utiliser la fonction put(), en transmettant la clé et la valeur
+    peopleAges.put("Barbara", 42)
+    peopleAges["Joe"] = 51
+    peopleAges["Fred"] = 31
+    println(peopleAges)
+}
---
+++ Utiliser LiveData avec ViewModel/readme.md
@@ -0,0 +1,2 @@
+# LiveData avec ViewModel
+
